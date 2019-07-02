6c2915292ae3de52443431ec39c979663fa71680
Your Name <you@example.com>
2019-07-02 12:58:27 -0500

    first commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,34 @@
+
+# for i in range(1,151):
+#     print(i)
+
+# for i in range(5,1005,5):
+#     print(i)
+
+# for i in range(1,101,1):
+#     print(i)
+#     if i%5 == 0:
+#         print("Coding")
+#     if i%10 == 0:
+#         print("Dojo")
+
+
+
+# sum = 0
+# for i in range(1,500000,1):
+#     if i%2 != 0:
+#         sum = sum + i 
+        
+# print(sum)
+
+# for i in range(2018,0,-4):
+#     if i%2 == 0:
+#         print(i)
+
+lowNum = 2
+highNum = 9 
+mult = 3
+
+for i in range(lowNum, highNum + 1):
+    if i% mult == 0:
+        print(i)
